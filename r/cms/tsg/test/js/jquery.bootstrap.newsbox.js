67c001c11d2957e7125280afe6a97eae83cf1f8a
fmailb <fmailb@aliyun.com>
2017-11-06 16:41:31 +0800

    添加footer二维码和row3的响应设计

--- r/cms/tsg/test/js/jquery.bootstrap.newsbox.js
+++ r/cms/tsg/test/js/jquery.bootstrap.newsbox.js
@@ -135,8 +135,8 @@ if (typeof Object.create !== 'function') {
             var panel = this.findPanelObject();
             if( panel ) {
                 var nav = '<div class="div-td-50"><ul class="pagination pull-right" style="margin: 0px;">' +
-                             '<li><a href="#" class="prev"><span class="glyphicon glyphicon-chevron-down"></span></a></li>' +
-                             '<li><a href="#" class="next"><span class="glyphicon glyphicon-chevron-up"></span></a></li>' +
+                             '<li><a href="#" class="prev"><span class="glyphicon glyphicon-triangle-bottom"></span></a></li>' +
+                             '<li><a href="#" class="next"><span class="glyphicon glyphicon-triangle-top"></span></a></li>' +
                            '</ul><div class="clearfix"></div></div>';
 
 
