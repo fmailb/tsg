197ff4f04f64949610686c17a05960346dabfecb
fmailb <fmailbbbb@gmail.com>
2017-11-16 21:44:27 +0800

    ie8的适配基本上做好了

--- r/cms/tsg/test/js/tsg2.js
+++ r/cms/tsg/test/js/tsg2.js
@@ -1,4 +1,53 @@
 /**
  * Created by fy on 2017/11/8.
  */
-// 尾部js
+// ie8、9 的js不支持filter方法，这里定义一个。
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function (fun /*, thisp */) {
+        "use strict";
+
+        if (this === void 0 || this === null)
+            throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== "function")
+            throw new TypeError();
+
+        var res = [];
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++) {
+            if (i in t) {
+                var val = t[i]; // in case fun mutates this
+                if (fun.call(thisp, val, i, t))
+                    res.push(val);
+            }
+        }
+
+        return res;
+    };
+}
+
+//也没有indexof 方法
+if (!Array.prototype.indexOf)
+{
+    Array.prototype.indexOf = function(elt /*, from*/)
+    {
+        var len = this.length >>> 0;
+
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+            ? Math.ceil(from)
+            : Math.floor(from);
+        if (from < 0)
+            from += len;
+
+        for (; from < len; from++)
+        {
+            if (from in this &&
+                this[from] === elt)
+                return from;
+        }
+        return -1;
+    };
+}
